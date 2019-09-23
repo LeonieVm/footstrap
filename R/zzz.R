e66263cc92548cca859a2e80a9d9948df36d2fc0
LeonieVm <l.v.d.e.vogelsmeier@tilburguniversity.edu>
2019-09-23 14:34:27 +0200

    changed name

--- R/zzz.R
+++ R/zzz.R
@@ -18,6 +18,6 @@
 .onAttach <- function(libname, pkgname) {
     # Print a welcome message when the package is attached.
     cat(rep("-", 20), "\n")
-    cat("It wooooorked `footstrap`!", "\n")
+    cat("It wooooorked again `footstrap`!", "\n")
     cat(rep("-", 20), "\n")
 }
